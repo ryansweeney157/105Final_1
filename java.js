7aff5b8e6adfe15f882fa31d741ac913af8000be
Tyler M. <tjmcdowell726@gmail.com>
2024-10-21 08:36:51 -0400

    Finished initial list creation

--- java.js
+++ java.js
@@ -1,9 +1,13 @@
 let initialList = []
 function askForListItems() {
     let interval = Number(prompt("How many items do you want to add?"));
+    while (isNaN(interval)) {
+        interval = Number(prompt("How many items do you want to add?"));
+    }
     for (let i = 0; i < interval; i++) {
         let item = prompt("What item do you want to add?");
-        initialList.push(toUpperCase(item));
+        initialList.push(item.toUpperCase());
         console.log(item + " added.");
     }
 }
+askForListItems()
